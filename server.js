1440863d2427067d060b8218674b721d160cf2c7
Jakub <jakub.bulhak@gmail.com>
2019-11-04 23:35:45 +0100

    Delete of console logs and commented out code.

--- server.js
+++ server.js
@@ -1,53 +1,22 @@
-var http = require("http");
-var dotenv = require("dotenv");
-var express = require("express");
-var app = express();
+const http = require("http");
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
 
 dotenv.config();
 
-var server = app.listen(
+const server = app.listen(
   process.env.PORT,
   console.log("Server is listening on port: ", process.env.PORT)
 );
-var socket = require("socket.io");
-var cors = require("cors");
-var io = require("socket.io").listen(server);
+const cors = require("cors");
+const io = require("socket.io").listen(server);
 app.use(cors());
-// var server = express();
-// server.use((req, res) => res.sendFile(INDEX));
-// server.listen(PORT, () => console.log("Listening on: ", PORT));
-// server.get("/", function(req, res) {
-//   res.sendfile(__dirname + "/index.html");
-// });
-// server = app.listen(PORT, function() {
-//   console.log(`server is running on port ${PORT}`);
-// });
-
-var rooms = ["general", "room1", "room2"];
-var usernames = [];
-
-// server.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://localhost:3000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept-Type"
-//   );
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
-
-// const io = socket(server);
-// var io = require("socket.io")(server);
-// app.listen(PORT);
-// server.listen(PORT);
-// io.set("origins", "localhost:3000");
+const rooms = ["general", "room1", "room2"];
+let usernames = [];
 
 io.on("connection", socket => {
   console.log(`connected user with id: ${socket.id}`);
-  // io.in("general").clients(function(error, clients) {
-  //   var numClients = clients;
-  //   console.log(numClients);
-  // });
 
   socket.on(
     "adduser",
@@ -57,15 +26,8 @@ io.on("connection", socket => {
       obj["nickname"] = username;
       obj["room"] = currentRoom;
       usernames.push(obj);
-      // usernames[String(socket.id)] = username;
-
-      console.log(usernames);
-      console.log("USERNAMES: ", usernames);
-      console.log(username);
       socket.room = "general";
       socket.join("general");
-      // usernames[String(username)] = socket.id;
-      //echo to client they've connected
       socket.emit(
         "updatechat",
         "SERVER",
@@ -83,9 +45,7 @@ io.on("connection", socket => {
     },
     () => {
       io.in(currentRoom).clients(function(error, clients) {
-        var numClients = clients;
-        console.log(clients);
-        console.log("numclients aduser: ", numClients);
+        let numClients = clients;
         let usersSocket = numClients;
 
         let filteredUsers = usernames.filter(item => {
@@ -95,7 +55,6 @@ io.on("connection", socket => {
             return newArray;
           }
         });
-        console.log("FILTERED TABLE: ", filteredUsers);
         socket.emit("SEND_ROOM_SOCKET_USERS", filteredUsers);
       });
     }
@@ -106,12 +65,10 @@ io.on("connection", socket => {
     //leave the current room (stored in session)
     socket.leave(socket.room);
     // join new room, recieved as function parameter
-    console.log(usernames);
     socket.join(newroom);
     //Find index of specific object using findIndex method.
     const userIndex = usernames.findIndex(user => user.nickname == username);
     usernames[userIndex].room = newroom;
-    console.log("USERNAMES AFTER UPDATEEEEE: ", usernames);
 
     socket.emit(
       "updatechat",
@@ -125,49 +82,31 @@ io.on("connection", socket => {
       .emit("updatechat", "SERVER", socket.username + " has joined this room.");
     socket.emit("updaterooms", rooms, newroom);
     socket.broadcast.to(newroom).emit("GET_ROOM_USERS", newroom);
-    // console.log(io.sockets.clients("general"));
   });
 
   socket.on("GET_ROOM_USERS", currentRoom => {
     io.in(currentRoom).clients(function(error, clients) {
       let usersSocket = clients;
-      console.log("users Socket: ", usersSocket);
       let filteredUsers = usernames.filter(item => {
         let newArray = [];
         for (let i = 0; i <= usernames.length; i++) {
           newArray.push(item.id == usersSocket[i]);
-          console.log("newArray", newArray);
           return newArray;
         }
       });
 
       socket.emit("SEND_ROOM_SOCKET_USERS", filteredUsers);
-      console.log("FILTERED TABLE: ", filteredUsers);
     });
-    // io.in(currentRoom).clients(function(error, clients) {
-    //   let usersSocket = clients;
-    //   console.log("users Socket: ", usersSocket);
-    //   let filteredUsers = [];
-    //   filteredUsers = usernames.filter(item => {
-    //     console.log(item.room == currentRoom);
-    //     return item.room == currentRoom;
-    //   });
-    //   socket.emit("SEND_ROOM_SOCKET_USERS", filteredUsers);
-    //   console.log("FILTERED TABLE: ", filteredUsers);
-    // });
-    console.log(`after get room users back`);
   });
 
   socket.on("disconnect", () => {
-    console.log(`disonnected user with id: ${socket.id}`);
     //remove the username from< global usernames list
     delete usernames[socket.id];
-    for (var i = 0; i < usernames.length; i++)
+    for (let i = 0; i < usernames.length; i++)
       if (usernames[i].id === socket.id) {
         usernames.splice(i, 1);
         break;
       }
-    console.log(usernames);
     //update list of users in chat, client-side
     io.sockets.emit("updateusers", usernames);
     //echo globally that this client has left
@@ -181,26 +120,13 @@ io.on("connection", socket => {
 
   socket.on("typing", data => {
     socket.broadcast.emit("typing", data.currentRoom);
-    console.log("pisze");
-    console.log(data);
-    console.log(socket.id);
-    console.log(socket.rooms);
-    //checking length of users to know if users are in the same room
-    // io.in(data.currentRoom).clients(function(error, clients) {
-    //   var numClients = clients.length;
-    //   var testClients = clients;
-    //   console.log(numClients);
-    //   console.log(testClients);
-    // });
   });
 
   socket.on("nottyping", data => {
     socket.broadcast.emit("nottyping", data);
-    console.log("nie pisze");
   });
 
   socket.on("SEND_MESSAGE", function(data) {
-    console.log(data);
     socket.to(data.currentRoom).emit("RECEIVE_MESSAGE", data);
   });
 });
